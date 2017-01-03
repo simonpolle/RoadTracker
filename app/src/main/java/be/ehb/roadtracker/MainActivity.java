17450f66dd9356fda5526058eac94129ecc0b4e9
Polak\Polako <simon.polle@student.ehb.be>
2017-01-03 11:09:14 +0100

    Initial commit

---
+++ app/src/main/java/be/ehb/roadtracker/MainActivity.java
@@ -0,0 +1,15 @@
+package be.ehb.roadtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
